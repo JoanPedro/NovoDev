5f84bf4ccf138996541c6efa674b96a1eeede310
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-05-13 12:13:06 -0300

    Adiciona Nodemon e Script

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const express = require('express')
 const app = express()
 
-app.get('/teste', (req, res) => {
-  return res.send('Hello World!')
+/* Query Params = http://localhost:3000/teste?user=Joan
+Adquirindo o Query. -> 
+const user = req.query.user ou 
+const { user } = req.query
+*/
+
+/* Route Params = http://localhost:3000/teste/1
+Adquirindo o Params. -> a rota precisa ('/teste/:id')
+const id = req.params.id ou
+const { id } = req.params
+*/
+
+/* Request Body 
+{
+  "name": "Joan Pedro",
+  "email": "joan@pedro.com.br"
+}
+*/
+
+const users = ['Joan', 'Carlos', 'Matheus']
+
+app.get('/teste/:index', (req, res) => {
+  const { index } = req.params
+
+  return res.json(users[index])
 })
 
-app.listen(3003)
+app.listen(3000)
