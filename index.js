5adce3ed52abc2bda8efe69a8b0dc785be88caaf
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-05-13 14:01:50 -0300

    Middlewares Locais e Globais

--- index.js
+++ index.js
@@ -22,38 +22,69 @@ const { id } = req.params
 }
 */
 
+// Middleware Global. Passa por todas as requisições
+
+app.use((req, res, next) => {
+  console.log('Request')
+  console.log(`Método: ${req.method}; Url: ${req.url}`)
+
+  next()
+})
+
+// Middleware Local. Específico para rotas.
+
+const existOrError = (req, res, next) => {
+
+  if(!req.body.name) {
+    return res.status(400).json({ error: "User name is required!"})
+  }
+
+  return next()
+}
+
+const verifyUserInArray = (req, res, next) => {
+  if(!users[req.params.index]) {
+    return res.status(400).json({ error: "User does not exists!"})
+  }
+  
+  return next()
+}
+
+// --------------------------------- //
+
 const users = ['Joan', 'Carlos', 'Matheus']
 
 app.get('/users', (req, res) => {
   return res.json(users)
 })
 
-app.get('/user/:index', (req, res) => {
+app.get('/user/:index', verifyUserInArray, (req, res) => {
   const { index } = req.params
 
   return res.json(users[index])
 })
 
-app.post('/users', (req, res) => {
+app.post('/users', existOrError, (req, res) => { // Com Middleware de Verificação
   const { name } = req.body
 
   users.push(name)
   return res.json(users)
 })
 
-app.delete('/users/:index', (req, res) => {
+app.delete('/users/:index', verifyUserInArray, (req, res) => {
   const { index } = req.params
 
   users.splice(index, 1)
   return res.json(users)
 })
 
-app.put('/users/:index', (req, res) => {
-  const { index } = req.params
-  const { name } = req.body
+app.put('/users/:index', existOrError, 
+  verifyUserInArray,(req, res) => { // Com Middleware de Verificação
+    const { index } = req.params
+    const { name } = req.body
 
-  users[index] = name
-  return res.json(users)
+    users[index] = name
+    return res.json(users)
 })
 
 app.listen(3000)
