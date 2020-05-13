a7a5258ba391f15a369ce9ffd107e5d4ae022f87
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-05-13 12:37:46 -0300

    Princípios da API

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json()) // Habilitar o Body no formato JSON
+
 /* Query Params = http://localhost:3000/teste?user=Joan
 Adquirindo o Query. -> 
 const user = req.query.user ou 
@@ -22,10 +24,36 @@ const { id } = req.params
 
 const users = ['Joan', 'Carlos', 'Matheus']
 
-app.get('/teste/:index', (req, res) => {
+app.get('/users', (req, res) => {
+  return res.json(users)
+})
+
+app.get('/user/:index', (req, res) => {
   const { index } = req.params
 
   return res.json(users[index])
+})
+
+app.post('/users', (req, res) => {
+  const { name } = req.body
+
+  users.push(name)
+  return res.json(users)
+})
+
+app.delete('/users/:index', (req, res) => {
+  const { index } = req.params
+
+  users.splice(index, 1)
+  return res.json(users)
+})
+
+app.put('/users/:index', (req, res) => {
+  const { index } = req.params
+  const { name } = req.body
+
+  users[index] = name
+  return res.json(users)
 })
 
 app.listen(3000)
